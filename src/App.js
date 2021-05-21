b1167c714c4e9bd5b9515e06076229b045a667d6
Kaje <59009825+KajeA@users.noreply.github.com>
2021-05-21 09:45:14 -0600

    finished styling

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1>Contact Directory</h1>
         <SearchBox
           placeholder="Search..."
           handleChange={e => this.setState({searchField: e.target.value})}
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,9 @@ export const Card = (props) => (
   <div className='card-container'>
     <img className="card-image" alt="contact" src={`https://picsum.photos/id/${props.contact.id * 7}/200`} />
     <h2>{props.contact.name}</h2>
+    <hr className="horilines"/>
     <p>{props.contact.phone}</p>
+    <hr className="horilines2"/>
     <p>{props.contact.email}</p>
   </div>
 )
